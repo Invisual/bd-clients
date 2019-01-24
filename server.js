df5bc4343f41a11f1c97fda6a1e6deb2d5c70ee5
nixton9 <eduardojoaoaraujo@gmail.com>
2019-01-24 09:51:55 +0000

    Configuração para login

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require('cors');
 require('dotenv').config()
 
 const clientRoutes = require('./routes/api/clients');
 
 const app = express();
+
 app.use(bodyParser.json());
-app.use('/api/clients', clientRoutes);
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
+app.use('/api/clients', clientRoutes);
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, 'client', 'build')));
