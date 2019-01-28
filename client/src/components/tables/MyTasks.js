37e150b2fd2c24fa709658f2912de7cc02fbbd41
DESKTOP-2RINU3H\Admin <pedro.fidalgo@invisual.pt>
2019-01-28 11:06:49 +0000

    Renovação de estilos 1-2

--- client/src/components/tables/MyTasks.js
+++ client/src/components/tables/MyTasks.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import SingleTask from '../singles/SingleTask';
+import { FiArrowRight } from 'react-icons/fi';
 
 export const MyTasks = (props) => {
     return (
-        <div className="mytasks-container widget">
+        <div className="mytasks-container widget cards-container">
           <h4 className="widget-title">{props.title}</h4>
           {props.tasks.map(task => {
-              return <SingleTask key={task.id} id={task.id} title={task.title} state={task.state} stateVal={task.stateval}/>
+              return <SingleTask key={task.id} id={task.id} title={task.title} state={task.state} hourState={task.hourState} projectState={task.projectState} stateVal={task.stateval}/>
           })}
+          <div className="see-all">Ver todas <span className="arrow"><FiArrowRight color="#0031e6"/></span></div>
         </div>
       );
 }
