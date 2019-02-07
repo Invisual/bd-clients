d808a45392895e7b251d83b39bbf7bd874764176
DESKTOP-2RINU3H\Admin <pedro.fidalgo@invisual.pt>
2019-02-07 14:30:20 +0000

    Componente e container de inserir tarefas criados

---
+++ client/src/containers/inserts/CreateTaskContainer.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import {CreateTask} from '../../components/inserts/CreateTask';
+
+class CreateTaskContainer extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            titleInput: '',
+            briefingInput: '',
+            deadlineInput: '',
+            billingInput: '',
+            clientInput: '',
+            accountInput: ''
+        }
+    }
+
+    changeTitleInput = (e) => {
+        this.setState({ titleInput: e.target.value })
+    }
+
+    changeBriefingInput = (e) => {
+        this.setState({ briefingInput: e.target.value })
+    }
+
+    changeDeadlineInput = (e) => {
+        this.setState({ deadlineInput: e.target.value })
+    }
+
+    changeBillingInput = (e) => {
+        this.setState({ billingInput: e.target.value })
+    }
+
+    changeClientInput = (e) => {
+        this.setState({ clientInput: e.target.value })
+    }
+
+    changeAccountInput = (e) => {
+        this.setState({ accountInput: e.target.value })
+    }
+
+
+    render(){
+        return <CreateTask
+                title={this.props.title}
+                changeTitleInput={this.changeTitleInput}
+                changeBriefingInput={this.changeBriefingInput}
+                changeDeadlineInput={this.changeDeadlineInput}
+                changeBillingInput={this.changeBillingInput}
+                changeClientInput={this.changeClientInput}
+                changeAccountInput={this.changeAccountInput}
+                />;
+    }
+}
+
+export default CreateTaskContainer;
