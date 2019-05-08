c9684f27520f75d934858377195350ae3bb2caa9
nixton9 <eduardojoaoaraujo@gmail.com>
2019-05-08 16:33:11 +0100

    Team Component

--- client/src/containers/lists/AllTeamContainer.js
+++ client/src/containers/lists/AllTeamContainer.js
@@ -2,6 +2,16 @@ import React, {Component} from 'react'
 
 
 class AllTeamContainer extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            members: []
+        }
+    }
+
+    render(){
+        return '<h1>Hello World</h1>'
+    }
 
 }
 
